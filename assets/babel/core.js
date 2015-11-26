b417561190ab946f2cd8b265c067713ba62f606b
katsew <y.katsew@gmail.com>
2015-11-26 20:02:27 +0900

    update application

--- assets/babel/core.js
+++ assets/babel/core.js
@@ -1,3 +1,26 @@
 "use strict";
 
-const httpClient = require("superagent");
+const $ = require('jquery');
+const ApiClient = require('./service/api-client.js');
+
+$(() => {
+  $('#sendPreregistration').on('click', (e) => {
+    e.preventDefault();
+    let data = $('#newRegistration').serialize();
+    ApiClient.sendRegistrationEmail(data, (err, res) => {
+      console.log('--- response comes here ---');
+      console.log(err);
+      console.log(res);
+    });
+  });
+
+  $('#sendLoginData').on('click', (e) => {
+    e.preventDefault();
+    let data = $('#login').serialize();
+    ApiClient.login(data, (err, res) => {
+      console.log('--- response comes here ---');
+      console.log(err);
+      console.log(res);
+    });
+  });
+});
---
+++ assets/babel/service/api-client.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const httpClient = require("superagent");
+const API_VERSION = "v1";
+const HOST = "localhost";
+const PORT = "3000";
+const parse = JSON.parse;
+const API_ENDPOINT = `http://${HOST}:${PORT}/api/${API_VERSION}`;
+
+class ApiClient {
+  constructor() {
+  }
+  sendRegistrationEmail(data, callback) {
+    httpClient
+      .post(`${API_ENDPOINT}/m/c`)
+      .send(data)
+      .end((err, res) => {
+        console.error(err);
+        console.log(res);
+        callback(err, parse(res.text));
+      });
+  }
+  login(data, callback) {
+    httpClient
+      .post(`${API_ENDPOINT}/login`)
+      .send(data)
+      .end((err, res) => {
+        console.error(err);
+        console.log(res);
+        callback(err, parse(res.text));
+      });
+  }
+};
+
+module.exports = new ApiClient();
