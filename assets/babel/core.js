c78c83200ce42a1cf09a81da15effe2d068f2ec8
katsew <y.katsew@gmail.com>
2015-11-27 16:56:58 +0900

    update application
    add redux ecosystems

--- assets/babel/core.js
+++ assets/babel/core.js
@@ -1,11 +1,36 @@
 "use strict";
 
+// React
 const React = require('react');
 const ReactDom = require('react-dom');
 const render = ReactDom.render;
-const Router = require('react-router').Router;
+
+// React Router
+// const Router = require('react-router').Router;
 const Route = require('react-router').Route;
-const Link = require('react-router').Link;
+
+// Redux Router
+const reduxReactRouter = require('redux-router').reduxReactRouter;
+const routerStateReducer = require('redux-router').routerStateReducer;
+const ReduxRouter = require('redux-router').ReduxRouter;
+const pushState = require('redux-router').pushState;
+
+// Redux
+const compose = require('redux').compose;
+const combineReducers = require('redux').combineReducers;
+const createStore = require('redux').createStore;
+
+// Redux Devtools
+const devTools = require('redux-devtools').devTools;
+const DevTools = require('redux-devtools/lib/react').DevTools
+const DebugPanel = require('redux-devtools/lib/react').DebugPanel
+const LogMonitor = require('redux-devtools/lib/react').LogMonitor
+
+// React Redux
+const Provider = require('react-redux').Provider;
+const connect = require('react-redux').connect;
+
+const createHistory = require('history').createHistory;
 const appContainer = document.getElementById('app');
 
 // React Components
@@ -23,14 +48,36 @@ const App = React.createClass({
     );
   }
 });
+connect(
+  state => ({ q: state.router.location.query.q}),
+  {pushState}
+)(App);
 
-render(
-  (
-    <Router>
-      <Route path="/" component={App}>
-        <Route path="registar" component={NewRegistration} />
-        <Route path="login" component={Login} />
-      </Route>
-    </Router>
-  )
-, appContainer);
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="registar" component={NewRegistration} />
+    <Route path="login" component={Login} />
+  </Route>
+);
+
+const reducer = combineReducers({
+  router: routerStateReducer
+});
+
+const store = compose(
+  reduxReactRouter({ createHistory }),
+  devTools()
+)(createStore)(reducer);
+
+render((
+  <div>
+    <Provider store={store}>
+      <ReduxRouter>
+        {routes}
+      </ReduxRouter>
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>
+), appContainer);
